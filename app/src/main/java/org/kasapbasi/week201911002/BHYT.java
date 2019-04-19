41c22a5ae548b409ae4209fdcd2e047e9bc6b89a
mckasap <mckasap@gmail.com>
2019-04-19 15:53:33 +0300

    BroadCast REceiver

--- app/src/main/java/org/kasapbasi/week201911002/BHYT.java
+++ app/src/main/java/org/kasapbasi/week201911002/BHYT.java
@@ -1,4 +1,19 @@
 package org.kasapbasi.week201911002;
 
-public class BHYT {
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class BHYT extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+   String str=intent.getExtras().getString("mac","Bulunamadı");
+
+        Toast.makeText(context,"Bu haftaki rakip "+str,Toast.LENGTH_LONG).show();
+    }
 }
