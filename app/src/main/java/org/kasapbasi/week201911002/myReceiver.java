63db263a5e1ccbb7b01d60231ab4093f9e803660
mckasap <mckasap@gmail.com>
2019-04-19 15:15:10 +0300

    BroadCast REceiver

--- app/src/main/java/org/kasapbasi/week201911002/myReceiver.java
+++ app/src/main/java/org/kasapbasi/week201911002/myReceiver.java
@@ -1,4 +1,30 @@
 package org.kasapbasi.week201911002;
 
-public class myReceiver {
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class myReceiver extends BroadcastReceiver {
+    public myReceiver() {
+        super();
+    }
+
+
+    @Override
+    public IBinder peekService(Context myContext, Intent service) {
+        return super.peekService(myContext, service);
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText( null," MESAJ GELDİ ",Toast.LENGTH_LONG).show();
+
+    }
 }
